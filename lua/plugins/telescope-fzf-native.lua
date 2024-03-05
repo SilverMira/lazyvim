a790c675deb89f49afe536d9ee3a130f6589dd35
SilverMira <66930495+SilverMira@users.noreply.github.com>
2024-03-05 16:43:14 +0800

    config: telescope-fzf-native
    Enable telescope-fzf-native.nvim with correct build shell commands
    using CMAKE

---
+++ lua/plugins/telescope-fzf-native.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = (function(_)
+      local build_command =
+        "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+      local shell = vim.fn.has("win32") == 1 and "cmd /c" or "sh -c"
+      return shell .. ' "' .. build_command .. '"'
+    end)(),
+    enabled = vim.fn.executable("cmake") == 1,
+  },
+}
