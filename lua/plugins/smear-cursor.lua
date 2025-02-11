ac318d9cc8431ce8ae69c845a3a14f8af15ce7a7
SilverMira <66930495+SilverMira@users.noreply.github.com>
2025-02-11 12:08:54 +0800

    config: smear condition

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -2,7 +2,7 @@ return {
   {
     "sphamba/smear-cursor.nvim",
     optional = true,
-    cond = os.getenv("SSH_TTY") ~= nil,
+    cond = os.getenv("SSH_TTY") == nil,
     opts = {
       stiffness = 0.9,
       trailing_stiffness = 0.6,
